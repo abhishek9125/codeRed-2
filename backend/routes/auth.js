f4a095a09fa2f4afe99f21038073f91dc39e9412
agarwal.a@mamaearth.in <aagarwal_be17@thapar.edu>
2023-04-01 01:21:41 +0530

    Added: Routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const { register } = require('../controllers/auth')
+const { register, verifyEmail, resendEmailVerificationToken, forgetPassword, verifyPasswordResetToken, signIn, resetPassword, isAuthVerification } = require('../controllers/auth');
+const { isAuth, isValidPasswordResetToken } = require('../middlewares/auth');
+const { userValidator, validate, validatePassword, signInValidator } = require('../middlewares/validator');
 
 const router = express.Router();
 
-router.post('/register', register);
-
+router.get('/is-auth', isAuth, isAuthVerification);
+router.post('/register', userValidator, validate, register);
+router.post('/sign-in', signInValidator, validate, signIn);
+router.post('/verify-email', verifyEmail);
+router.post('/resend-email-verification-token', resendEmailVerificationToken);
+router.post('/forget-password', forgetPassword);
+router.post('/verify-password-reset-token', isValidPasswordResetToken, verifyPasswordResetToken);
+router.post('/reset-password', validatePassword, validate, isValidPasswordResetToken, resetPassword);
 
 module.exports = router;

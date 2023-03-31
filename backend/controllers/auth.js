f20ffe8ff1ef65de74ec3c6dfaf763884ceee280
agarwal.a@mamaearth.in <aagarwal_be17@thapar.edu>
2023-04-01 01:20:06 +0530

    Added: Reset Token Controllers

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
 const { isValidObjectId } = require('mongoose');
 
-const { sendError } = require('../utils/helper');
+const { sendError, generateRandomByte } = require('../utils/helper');
 const { generateOTP, generateMailTransporter } = require('../utils/mail');
 
 const User = require('../models/user');
 const EmailVerificationToken = require('../models/emailVerificationToken');
+const PasswordResetToken = require('../models/passwordResetToken');
 
 exports.register = async (req, res) => {
 
@@ -52,29 +53,29 @@ exports.verifyEmail = async (req, res) => {
 
     const { userId, OTP } = req.body;
 
-    if(!isValidObjectId(userId)) {
+    if (!isValidObjectId(userId)) {
         return sendError(res, 'Invalid User');
     }
 
     const user = await User.findById(userId);
 
-    if(!user) {
+    if (!user) {
         return sendError(res, 'User Not Found', 404);
     }
 
-    if(user.isVerified) {
+    if (user.isVerified) {
         return sendError(res, 'User is already Verified..!!');
     }
 
     const token = await EmailVerificationToken.findOne({ owner: userId });
 
-    if(!token) {
+    if (!token) {
         return sendError(res, 'Token Not Found');
     }
 
     const isTokenValid = token.compareToken(OTP);
 
-    if(!isTokenValid) {
+    if (!isTokenValid) {
         return sendError(res, 'OTP is Invalid');
     }
 
@@ -115,16 +116,16 @@ exports.resendEmailVerificationToken = async (req, res) => {
 
     const user = await User.findById(userId);
 
-    if(!user) {
+    if (!user) {
         return sendError(res, 'User Not Found');
     }
 
-    if(user.isVerified) {
+    if (user.isVerified) {
         return sendError(res, 'User is already Verified..!!');
     }
 
     const alreadyHasToken = await EmailVerificationToken.findOne({ owner: userId });
-    if(alreadyHasToken) {
+    if (alreadyHasToken) {
         await EmailVerificationToken.findByIdAndDelete(alreadyHasToken._id);
     }
 
@@ -142,12 +143,113 @@ exports.resendEmailVerificationToken = async (req, res) => {
             <p> Your Verification OTP </p>
             <h1> ${OTP} </h1>
         `
-    })
+    });
 
     return res.status(201).json({
         message: "Please check your email. OTP has been sent to your email account."
-    })
+    });
+}
 
+exports.forgetPassword = async (req, res) => {
+
+    const { email } = req.body;
+
+    if (!email) {
+        return sendError(res, 'Email is Missing');
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        return sendError(res, 'User Not Found', 404);
+    }
+
+    const alreadyHasToken = await PasswordResetToken.findOne({ owner: user._id });
+
+    if (alreadyHasToken) {
+        await PasswordResetToken.findByIdAndDelete(alreadyHasToken._id);
+    }
+
+    const token = await generateRandomByte();
+
+    const newPasswordResetToken = await PasswordResetToken({ owner: user._id, token });
+    await newPasswordResetToken.save();
+
+    const resetPasswordUrl = `http://localhost:3000/auth/reset-password?token=${token}&id=${user._id}`;
+
+    var transport = generateMailTransporter();
+
+    transport.sendMail({
+        from: 'security@codered.com',
+        to: user.email,
+        subject: 'Reset Password Link',
+        html: `
+            <p> Click Here To Reset Password </p>
+            <a href='${resetPasswordUrl}'>Change Password</a>
+        `
+    });
+
+    return res.json({ message: 'Link sent to Your Email' });
+}
+
+exports.verifyPasswordResetToken = (req, res) => {
+    return res.json({ valid: true })
+}
+
+exports.resetPassword = async (req, res) => {
+    
+    const { newPassword, userId } = req.body;
+
+    const user = await User.findById(userId);
+
+    const isMatched = await User.comparePassword(newPassword);
+
+    if (isMatched) {
+        return sendError(res, 'New Password must be different from the old Password');
+    }
+
+    user.password = newPassword; // Password is Automatically Crypted by Modify Function in User Model
+    await user.save();
+
+    await PasswordResetToken.findByIdAndDelete(req.resetToken._id);
+
+    return res.json({ message: 'Password reset successful, now you can use your new password.' });
+}
+
+exports.signIn = async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        return sendError(res, 'User with following Email does not exist.');
+    }
+
+    const matched = await user.comparePassword(password);
+
+    if (!matched) {
+        return sendError(res, 'Email/Password not Matched');
+    }
+
+    const { _id, name, role, isVerified } = user;
+
+    const jwtToken = jwt.sign({ userId: _id }, process.env.JWT_SECRET);
+
+    res.json({ user: { id: _id, name, email, role, token: jwtToken, isVerified } });
+
+}
+
+exports.isAuthVerification = (req, res) => {
+    const { user } = req;
+    res.json({
+        user: {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            isVerified: user.isVerified,
+            role: user.role,
+        },
+    });
 }
 
 
